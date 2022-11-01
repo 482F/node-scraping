04c4a29bccee77d720ca92e7f6032ec8ee62b7da
482F <482F.git.mail@482f.net>
2022-11-02 00:00:00 +0900

    pso2wikiの雑談掲示板を取得するスクリプトを追加

---
+++ pso2-wiki-zatsudan.spec.js
@@ -0,0 +1,27 @@
+import { describe, expect, test } from 'vitest'
+import get from './pso2-wiki-zatsudan.js'
+import * as history from './history.js'
+
+describe('pso2-wiki-zatsudan', () => {
+  test('main', async () => {
+    const dbName = 'pso2-wiki-zatsudan-test'
+
+    const data = await get(false, dbName)
+    expect(data.items.length).not.toBe(0)
+    expect(data.text).toBeTruthy()
+
+    const registeredData = await get(true, dbName)
+    expect(registeredData.items.length).not.toBe(0)
+    expect(registeredData.text).toBeTruthy()
+
+    const filteredData = await get(true, dbName)
+    expect(filteredData.items.length).toBe(0)
+    expect(filteredData.text).toBeFalsy()
+
+    await history.deleteDb(dbName)
+  }, 1000 * 60 * 5)
+})
+
+
+
+
