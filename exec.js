803a98921b43e311efb1bc274bfb2be949ffd3a4
482F <482F.git.mail@482f.net>
2022-11-05 00:00:00 +0900

    通常のエラーは throw されるように

--- exec.js
+++ exec.js
@@ -2,7 +2,10 @@
   const target = process.argv[2]
   const targetFunc = await import(`./${target}.js`)
     .then((m) => m.default)
-    .catch(() => null)
+    .catch((e) => {
+      if (e.message.match(/^Cannot find module/)) return
+      else throw e
+    })
   if (!targetFunc) return
 
   const result = await targetFunc().then((r) => r.text)
