af48a1b0e7562a3d82ca8dce23d3b2baa97f6a1b
482F <0ryonryoko0@gmail.com>
2022-11-07 00:00:00 +0900

    url のパース結果を修正

--- pso2-wiki-zatsudan.js
+++ pso2-wiki-zatsudan.js
@@ -36,7 +36,7 @@ async function getComments(domain) {
       body: line
         .replace(/^-+/, '')
         .replace(/ --  &new{\d+-\d+-\d+ \(.\) \d+:\d+:\d+};$/, '')
-        .replaceAll(/\[\[(.+?)>(.+?)\]\]/g, '$1 ($2)'),
+        .replaceAll(/\[\[(.+?)>(.+?)\]\]/g, '$1 ($2) '),
       children: [],
     }
     parents[depth].children.push(comment)
