99a3762a2f64ea207f9e44e9bb3638fb43ddc2a6
482F <0ryonryoko0@gmail.com>
2022-11-10 00:00:00 +0900

    [feat] pso2ngs の雷雨情報を取得するスクリプトを追加

---
+++ pso2-thunderstorm.spec.js
@@ -0,0 +1,26 @@
+import { describe, expect, test } from 'vitest'
+import get from './pso2-thunderstorm.js'
+import * as history from './history.js'
+
+describe('pso2-thunderstorm', () => {
+  test('main', async () => {
+    const dbName = 'pso2-thunderstorm-test'
+
+    const data = await get(false, dbName)
+    expect(data.items.length).not.toBe(0)
+    expect(data.text).toBeTruthy()
+
+    const registeredData = await get(true, dbName)
+    expect(registeredData.items.length).not.toBe(0)
+    expect(registeredData.text).toBeTruthy()
+
+    const filteredData = await get(true, dbName)
+    expect(filteredData.items.length).toBe(0)
+    expect(filteredData.text).toBeFalsy()
+
+    await history.deleteDb(dbName)
+  }, 1000 * 60 * 5)
+})
+
+
+
